dfcf475c24bf98bdad73e07a6c72608bfe28b4b8
samane ghodrati <samane.ghodrati@gmail.com>
2025-04-15 08:00:15 -0900

    add

---
+++ quiz1.js
@@ -0,0 +1,13 @@
+/*
+Write a Node.js script that utilizes the fs module to perform the following tasks:
+
+1- Include the 'fs' module.
+2- Your solution should use the fs.writeFile() function 
+3- Write data to a file named 'example.txt'.
+4- The content to be written to the file should be: "This is the data to write to the file."
+5- Ensure that any errors encountered during the file writing process are appropriately handled and logged.
+6- Display a message to the console indicating that the data has been successfully written to the file.
+
+
+*/
+// answer
